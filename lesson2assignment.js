cef6299c665e9579f1110746f8df3cb738a992e6
aliceshanitah <aliceshanitah@gmail.com>
2020-04-02 23:14:03 -0700

    use of if else

---
+++ lesson2assignment.js
@@ -0,0 +1,16 @@
+const marks=50;
+if (marks>=80) { console.log("you got A");
+}
+else if(marks>=65){
+    console.log("you got B");
+}
+else if(marks>=50){
+    console.log("you passed");
+}
+else if(marks<50){
+    console.log("you failed");
+}
+
+    
+
+
